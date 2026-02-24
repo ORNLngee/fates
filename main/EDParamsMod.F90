JunyanDing/FATES_COMPASS

Dev v1 Mar.24 2022

main/EDParamsMod.F90
@@ -90,6 +90,13 @@ module EDParamsMod
    ! to be used at each node (compartment/organ)
    ! 1  = Christofferson et al. 2016 (TFS),   2 = Van Genuchten 1980
    integer, protected,allocatable,public :: hydr_htftype_node(:) 
+
+   ! Junyan added a new parameter to store the salinity file name    
+   real(r8),protected,public :: sal_sid 
+   character(len=param_string_length),parameter,public :: fates_name_sal_sid = "fates_sal_sid"
+   real(r8),protected,public :: sal_fid 
+   character(len=param_string_length),parameter,public :: fates_name_sal_fid = "fates_sal_fid"      
+   
    
    character(len=param_string_length),parameter,public :: ED_name_vai_top_bin_width = "fates_vai_top_bin_width"
    character(len=param_string_length),parameter,public :: ED_name_vai_width_increase_factor = "fates_vai_width_increase_factor"
@@ -255,6 +262,8 @@ contains
     theta_cj_c3                           = nan
     theta_cj_c4                           = nan
     dev_arbitrary                         = nan
+    sal_sid                               = nan             ! Junyan added, set the char variable to be empty
+    sal_fid                               = nan    
   end subroutine FatesParamsInit
 
   !-----------------------------------------------------------------------
@@ -289,6 +298,14 @@ contains
     call fates_params%RegisterParameter(name=ED_name_vai_width_increase_factor, dimension_shape=dimension_shape_scalar, &
          dimension_names=dim_names_scalar)
     
+    ! Junyan added 
+    call fates_params%RegisterParameter(name=fates_name_sal_sid, dimension_shape=dimension_shape_scalar, &
+         dimension_names=dim_names_scalar)
+         
+    call fates_params%RegisterParameter(name=fates_name_sal_fid, dimension_shape=dimension_shape_scalar, &
+         dimension_names=dim_names_scalar)
+         
+
     call fates_params%RegisterParameter(name=ED_name_photo_temp_acclim_timescale, dimension_shape=dimension_shape_scalar, &
          dimension_names=dim_names_scalar)
 
@@ -453,6 +470,7 @@ contains
   
   !-----------------------------------------------------------------------
   subroutine FatesReceiveParams(fates_params)
+    ! fucntion to receive parameter values 
     
     use FatesParametersInterface, only : fates_parameters_type, dimension_name_scalar
 
@@ -469,6 +487,15 @@ contains
     call fates_params%RetreiveParameter(name=ED_name_vai_width_increase_factor, &
          data=vai_width_increase_factor)
 
+   ! Junyan added, need to use the right Retrive subroutine, for integer, receive data as real, then convert to integer using nint() function
+    call fates_params%RetreiveParameter(name=fates_name_sal_fid, &
+         data=tmpreal)
+    sal_fid = int(tmpreal)
+    
+    call fates_params%RetreiveParameter(name=fates_name_sal_sid, &
+         data=tmpreal)                  
+    sal_sid = int(tmpreal)
+         
     call fates_params%RetreiveParameter(name=ED_name_photo_temp_acclim_timescale, &
          data=photo_temp_acclim_timescale)
 
@@ -649,6 +676,8 @@ contains
         
         write(fates_log(),*) '-----------  FATES Scalar Parameters -----------------'
         write(fates_log(),fmt0) 'vai_top_bin_width = ',vai_top_bin_width
+        write(fates_log(),fmt0) 'sal_fid = ',sal_fid
+        write(fates_log(),fmt0) 'sal_sid = ',sal_sid              
         write(fates_log(),fmt0) 'vai_width_increase_factor = ',vai_width_increase_factor
         write(fates_log(),fmt0) 'photo_temp_acclim_timescale = ',photo_temp_acclim_timescale
         write(fates_log(),fmti) 'hydr_htftype_node = ',hydr_htftype_node

main/EDPftvarcon.F90
@@ -197,7 +197,15 @@ module EDPftvarcon
      real(r8), allocatable :: hydr_rfrac_stem(:)    ! fraction of total tree resistance from troot to canopy
      real(r8), allocatable :: hydr_avuln_gs(:)      ! shape parameter for stomatal control of water vapor exiting leaf
      real(r8), allocatable :: hydr_p50_gs(:)        ! water potential at 50% loss of stomatal conductance
-     real(r8), allocatable :: hydr_k_lwp(:)         ! inner leaf humidity scaling coefficient 
+     real(r8), allocatable :: hydr_k_lwp(:)         ! inner leaf humidity scaling coefficient
+     ! Junyan added
+     real(r8), allocatable :: hydr_k_salex(:)       ! root salt exclusion, 0 - complete exclusion, 1 - no exclusion
+     real(r8), allocatable :: hydr_frt_loss_coe(:)  ! coefficient of root loss function due to soil saturation
+     real(r8), allocatable :: hydr_frt_loss_exp(:)  ! exponent of root loss function due to soil saturation
+     real(r8), allocatable :: hydr_frt_loss_se0(:)  ! the critical relative soil water content fine root mortality start to increase
+                                                    ! due to anoxia, se = theta - theta_res / theta_sat - theta_res     
+          
+      
 
      ! PFT x Organ Dimension  (organs are: 1=leaf, 2=stem, 3=transporting root, 4=absorbing root)
      ! ----------------------------------------------------------------------------------
@@ -465,6 +473,23 @@ contains
     name = 'fates_hydr_k_lwp'
     call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
          dimension_names=dim_names, lower_bounds=dim_lower_bound)
+
+    ! Junyan added         
+    name = 'fates_hydr_k_salex'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+         
+    name = 'fates_hydr_frt_loss_coe'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+         
+    name = 'fates_hydr_frt_loss_exp'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+         
+    name = 'fates_hydr_frt_loss_se0'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)                                       
 
     name = 'fates_mort_bmort'
     call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
@@ -802,7 +827,24 @@ contains
     name = 'fates_hydr_k_lwp'
     call fates_params%RetreiveParameterAllocate(name=name, &
          data=this%hydr_k_lwp)
-
+         
+   ! Junyan
+    name = 'fates_hydr_k_salex'
+    call fates_params%RetreiveParameterAllocate(name=name, &
+         data=this%hydr_k_salex)
+         
+    name = 'fates_hydr_frt_loss_coe'
+    call fates_params%RetreiveParameterAllocate(name=name, &
+         data=this%hydr_frt_loss_coe)
+         
+    name = 'fates_hydr_frt_loss_exp'
+    call fates_params%RetreiveParameterAllocate(name=name, &
+         data=this%hydr_frt_loss_exp)         
+                              
+    name = 'fates_hydr_frt_loss_se0'
+    call fates_params%RetreiveParameterAllocate(name=name, &
+         data=this%hydr_frt_loss_se0)  
+         
     name = 'fates_mort_bmort'
     call fates_params%RetreiveParameterAllocate(name=name, &
          data=this%bmort)
@@ -1435,7 +1477,7 @@ contains
         write(fates_log(),fmt0) 'phen_cold_size_threshold = ',EDPftvarcon_inst%phen_cold_size_threshold
         write(fates_log(),fmt0) 'phen_stem_drop_fraction',EDpftvarcon_inst%phen_stem_drop_fraction
         write(fates_log(),fmt0) 'fire_alpha_SH = ',EDPftvarcon_inst%fire_alpha_SH
-	write(fates_log(),fmt0) 'allom_frbstor_repro = ',EDPftvarcon_inst%allom_frbstor_repro
+	      write(fates_log(),fmt0) 'allom_frbstor_repro = ',EDPftvarcon_inst%allom_frbstor_repro
         write(fates_log(),fmt0) 'hydr_p_taper = ',EDPftvarcon_inst%hydr_p_taper
         write(fates_log(),fmt0) 'hydr_rs2 = ',EDPftvarcon_inst%hydr_rs2
         write(fates_log(),fmt0) 'hydr_srl = ',EDPftvarcon_inst%hydr_srl
@@ -1444,6 +1486,10 @@ contains
         write(fates_log(),fmt0) 'hydr_k_lwp = ',EDPftvarcon_inst%hydr_k_lwp
         write(fates_log(),fmt0) 'hydr_p50_gs = ',EDPftvarcon_inst%hydr_p50_gs
         write(fates_log(),fmt0) 'hydr_k_lwp = ',EDPftvarcon_inst%hydr_k_lwp
+        write(fates_log(),fmt0) 'hydr_k_salex = ',EDPftvarcon_inst%hydr_k_salex        
+        write(fates_log(),fmt0) 'hydr_frt_loss_coe = ',EDPftvarcon_inst%hydr_frt_loss_coe
+        write(fates_log(),fmt0) 'hydr_frt_loss_exp = ',EDPftvarcon_inst%hydr_frt_loss_exp
+        write(fates_log(),fmt0) 'hydr_frt_loss_se0 = ',EDPftvarcon_inst%hydr_frt_loss_se0         
         write(fates_log(),fmt0) 'hydr_avuln_node = ',EDPftvarcon_inst%hydr_avuln_node
         write(fates_log(),fmt0) 'hydr_p50_node = ',EDPftvarcon_inst%hydr_p50_node
         write(fates_log(),fmt0) 'hydr_thetas_node = ',EDPftvarcon_inst%hydr_thetas_node

ORNLngee/fates

(cherry picked from commit 2eef9702600ed42a17328d41ed911b839f21fc2f)

main/EDParamsMod.F90
@@ -125,6 +125,12 @@ module EDParamsMod
    ! to be used at each node (compartment/organ)
    ! 1  = Christofferson et al. 2016 (TFS),   2 = Van Genuchten 1980
    
+   ! Junyan added a new parameter to store the salinity file name    
+   real(r8),protected,public :: sal_sid 
+   character(len=param_string_length),parameter,public :: fates_name_sal_sid = "fates_sal_sid"
+   real(r8),protected,public :: sal_fid 
+   character(len=param_string_length),parameter,public :: fates_name_sal_fid = "fates_sal_fid"      
+
    character(len=param_string_length),parameter,public :: ED_name_sdlng_emerg_h2o_timescale = "fates_trs_seedling_emerg_h2o_timescale"
    character(len=param_string_length),parameter,public :: ED_name_sdlng_mort_par_timescale = "fates_trs_seedling_mort_par_timescale"
    character(len=param_string_length),parameter,public :: ED_name_sdlng_mdd_timescale = "fates_trs_seedling_mdd_timescale"
@@ -348,6 +354,8 @@ module EDParamsMod
     landuse_grazing_maxheight             = nan
     landuse_grazing_rate(:)               = nan
 
+    sal_sid                               = nan             ! Junyan added, set the char variable to be empty
+    sal_fid                               = nan    
   end subroutine FatesParamsInit
 
   !-----------------------------------------------------------------------
@@ -378,6 +386,13 @@ module EDParamsMod
     
     call FatesParamsInit()
 
+    ! Junyan added 
+    call fates_params%RegisterParameter(name=fates_name_sal_sid, dimension_shape=dimension_shape_scalar, &
+         dimension_names=dim_names_scalar)
+         
+    call fates_params%RegisterParameter(name=fates_name_sal_fid, dimension_shape=dimension_shape_scalar, &
+         dimension_names=dim_names_scalar)
+         
     call fates_params%RegisterParameter(name=ED_name_photo_temp_acclim_timescale, dimension_shape=dimension_shape_scalar, &
          dimension_names=dim_names_scalar)
 
@@ -587,6 +602,16 @@ module EDParamsMod
     real(r8), allocatable :: tmp_vector_by_landuse2(:)  ! local real vector for changing type on read
     real(r8), allocatable :: tmp_vector_by_landuse3(:)  ! local real vector for changing type on read
     real(r8), allocatable :: tmp_vector_by_landuse4(:)  ! local real vector for changing type on read
+
+    ! Junyan added, need to use the right Retrive subroutine, for integer, receive data as real, then convert to integer using nint() function
+    ! (CHECKING is needed here)
+    call fates_params%RetrieveParameter(name=fates_name_sal_fid, &
+         data=tmpreal)
+         sal_fid = int(tmpreal)
+
+    call fates_params%RetrieveParameter(name=fates_name_sal_sid, &
+         data=tmpreal)
+         sal_sid = int(tmpreal)
 
     call fates_params%RetrieveParameter(name=ED_name_photo_temp_acclim_timescale, &
          data=photo_temp_acclim_timescale)
@@ -815,6 +840,8 @@ module EDParamsMod
         
         write(fates_log(),*) '-----------  FATES Scalar Parameters -----------------'
         write(fates_log(),fmt0) 'vai_top_bin_width = ',vai_top_bin_width
+        write(fates_log(),fmt0) 'sal_fid = ',sal_fid
+        write(fates_log(),fmt0) 'sal_sid = ',sal_sid              
         write(fates_log(),fmt0) 'vai_width_increase_factor = ',vai_width_increase_factor
         write(fates_log(),fmt0) 'photo_temp_acclim_timescale = ',photo_temp_acclim_timescale
         write(fates_log(),fmt0) 'sdlng_emerg_h2o_timescale = ', sdlng_emerg_h2o_timescale

main/EDPftvarcon.F90
@@ -230,6 +230,14 @@ module EDPftvarcon
      real(r8), allocatable :: hydr_avuln_gs(:)      ! shape parameter for stomatal control of water vapor exiting leaf
      real(r8), allocatable :: hydr_p50_gs(:)        ! water potential at 50% loss of stomatal conductance
      real(r8), allocatable :: hydr_k_lwp(:)         ! inner leaf humidity scaling coefficient 
+     ! Junyan added
+     real(r8), allocatable :: hydr_k_salex(:)       ! root salt exclusion, 0 - complete exclusion, 1 - no exclusion
+     real(r8), allocatable :: hydr_frt_loss_coe(:)  ! coefficient of root loss function due to soil saturation
+     real(r8), allocatable :: hydr_frt_loss_exp(:)  ! exponent of root loss function due to soil saturation
+     real(r8), allocatable :: hydr_frt_loss_se0(:)  ! the critical relative soil water content fine root mortality start to increase
+                                                    ! due to anoxia, se = theta - theta_res / theta_sat - theta_res     
+          
+      
 
      ! PFT x Organ Dimension  (organs are: 1=leaf, 2=stem, 3=transporting root, 4=absorbing root)
      ! ----------------------------------------------------------------------------------
@@ -504,6 +512,23 @@ contains
     name = 'fates_hydro_k_lwp'
     call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
          dimension_names=dim_names, lower_bounds=dim_lower_bound)
+
+    ! Junyan added         
+    name = 'fates_hydro_k_salex'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+         
+    name = 'fates_hydro_frt_loss_coe'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+         
+    name = 'fates_hydro_frt_loss_exp'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+         
+    name = 'fates_hydro_frt_loss_se0'
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)                                       
 
     name = 'fates_mort_bmort'
     call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
@@ -913,7 +938,24 @@ contains
     name = 'fates_hydro_k_lwp'
     call fates_params%RetrieveParameterAllocate(name=name, &
          data=this%hydr_k_lwp)
-
+         
+   ! Junyan
+    name = 'fates_hydro_k_salex'
+    call fates_params%RetrieveParameterAllocate(name=name, &
+         data=this%hydr_k_salex)
+         
+    name = 'fates_hydro_frt_loss_coe'
+    call fates_params%RetrieveParameterAllocate(name=name, &
+         data=this%hydr_frt_loss_coe)
+         
+    name = 'fates_hydro_frt_loss_exp'
+    call fates_params%RetrieveParameterAllocate(name=name, &
+         data=this%hydr_frt_loss_exp)         
+                              
+    name = 'fates_hydro_frt_loss_se0'
+    call fates_params%RetrieveParameterAllocate(name=name, &
+         data=this%hydr_frt_loss_se0)  
+         
     name = 'fates_mort_bmort'
     call fates_params%RetrieveParameterAllocate(name=name, &
          data=this%bmort)
@@ -1651,6 +1693,10 @@ contains
         write(fates_log(),fmt0) 'hydro_fcap_node = ',EDPftvarcon_inst%hydr_fcap_node
         write(fates_log(),fmt0) 'hydro_pinot_node = ',EDPftvarcon_inst%hydr_pinot_node
         write(fates_log(),fmt0) 'hydro_kmax_node = ',EDPftvarcon_inst%hydr_kmax_node
+        write(fates_log(),fmt0) 'hydro_k_salex = ',EDPftvarcon_inst%hydr_k_salex
+        write(fates_log(),fmt0) 'hydro_frt_loss_coe = ',EDPftvarcon_inst%hydr_frt_loss_coe
+        write(fates_log(),fmt0) 'hydro_frt_loss_exp = ',EDPftvarcon_inst%hydr_frt_loss_exp
+        write(fates_log(),fmt0) 'hydro_frt_loss_se0 = ',EDPftvarcon_inst%hydr_frt_loss_se0
         write(fates_log(),fmt0) 'hlm_pft_map = ', EDPftvarcon_inst%hlm_pft_map
         write(fates_log(),fmt0) 'hydro_vg_alpha_node  = ',EDPftvarcon_inst%hydr_vg_alpha_node
         write(fates_log(),fmt0) 'hydro_vg_m_node  = ',EDPftvarcon_inst%hydr_vg_m_node